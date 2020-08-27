a30b7d91bfc49b221108baeb0d61fbfd08dcc6e2
kaankaray <kaankaray99@gmail.com>
2020-08-28 00:21:58 +0300

    Added attributes to TaggingModel. Every tag can have their own string attributes now.

--- Sources/Tagging/TaggingModel.swift
+++ Sources/Tagging/TaggingModel.swift
@@ -3,4 +3,6 @@ import Foundation
 public struct TaggingModel {
     public var text: String
     public var range: NSRange
+    public var attribute: [NSAttributedString.Key: Any]
+    public var symbolAttribute: [NSAttributedString.Key: Any]
 }
